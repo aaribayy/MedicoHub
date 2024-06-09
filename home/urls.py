352b54100628331d94981db46cfbd67bc31e0797
Ariba Azam <ariba.azam640@gmail.com>
2024-06-09 17:15:35 +0500

    updated urls.py

--- home/urls.py
+++ home/urls.py
@@ -13,11 +13,8 @@ urlpatterns = [
     path('signup/',views.SignUp, name='signup'),
     path('logout/',views.LogoutPage,name='logout'),
     path('userProfile/',views.user_profile,name='profile'),
-<<<<<<< HEAD
     
     
-=======
     path('profile/',views.profile, name="MyuserProfile"),
     path('chat/', views.chatbot, name='chat'),
->>>>>>> dd2c595c54982b9616578a755f1eb90fe1c0c6b9
 ]
